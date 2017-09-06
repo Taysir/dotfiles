6a817380699881eb6043c29999b944c7db20a500
Taysir of Rabiah <Taysir@users.noreply.github.com>
2017-09-06 22:39:46 +0900

    [change] Refactor bootstrapping Scoop.

--- scoop/bootstrap.cmd
+++ scoop/bootstrap.cmd
@@ -1,9 +1,7 @@
 @echo off
 setlocal
 
-SET DIR=%~dp0%
-
-powershell -NoProfile -ExecutionPolicy RemoteSigned -File %~dp0%install.ps1
+powershell -NoProfile -ExecutionPolicy RemoteSigned -File %~dp0%bootstrap.ps1
 
 endlocal
 exit /b
